0eecc6943ee95918558ecc32f6e7a623df5b4ea1
Nicole Avila <avila.nicky89@gmail.com>
2024-09-10 10:48:59 +0200

    some changes added to test suite 01

--- tests/testsuite01.spec.ts
+++ tests/testsuite01.spec.ts
@@ -12,6 +12,12 @@ test.describe('Test suite 01', () => {
    await page.locator('input[type="text"]').fill('tester01');
    await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
    await page.getByRole('button', { name: 'Login' }).click();
+// log out
+ // perform logout
+ await page.getByRole('button', { name: 'Logout' }).click();
+ expect(page.url()).toBe('http://localhost:3000/login'); //assertion
+ await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); //assertion
+
 
   });
 
