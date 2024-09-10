d567952ca60f982d6b20a3bf3020e228bc9015e7
Nicole Avila <avila.nicky89@gmail.com>
2024-09-10 08:25:43 +0200

    starting dependencies in project01

---
+++ tests/testsuite01.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Test suite 01', () => {
+
+  test('Test case 01', async ({ page }) => {
+    //perform log in
+   await page.goto('https://playwright.dev/'); await page.goto('http://localhost:3000');
+   expect(page.url()).toBe('http://localhost:3000/login'); 
+   //assertion
+   await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); 
+   //assertion
+   await page.locator('input[type="text"]').fill('tester01');
+   await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
+   await page.getByRole('button', { name: 'Login' }).click();
+
+  });
+
+
+});
+
